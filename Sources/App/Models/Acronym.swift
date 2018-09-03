1c77900ca482ad6a8d4bf0a85a44deaadb03e279
leiguang <575403441@qq.com>
2018-09-04 02:31:44 +0800

    Models And Fluent

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,41 @@
+//
+//  Acronym.swift
+//  App
+//
+//  Created by 雷广 on 2018/9/4.
+//
+
+import FluentSQLite
+import Vapor
+
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+//struct Acronym: Codable {
+//    var id: Int?
+//    var short: String
+//    var long: String
+//
+//    init(short: String, long: String) {
+//        self.short = short
+//        self.long = long
+//    }
+//}
+
+//extension Acronym: Model {
+//    typealias Database = SQLiteDatabase
+//    typealias ID = Int
+//    static let idKey: IDKey = \.id
+//}
+
+extension Acronym: SQLiteModel {}
+extension Acronym: Content {}
+extension Acronym: Migration {}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,4 +12,54 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    
+    // leiguang practices
+    
+    router.get("hello", "vapor") { req in
+        return "Hello Vapor"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello \(name)!"
+    }
+    
+    router.post(InfoData.self, at: "info") { (req, data) in
+        return "Hello \(data.name)"
+    }
+    
+    router.post(InfoData.self, at: "info/json") { (req, data) in
+        return InfoResponse(request: data)
+    }
+    
+    router.get("date") { req in
+        return "\(Date())"
+    }
+    
+    router.get("counter", Int.parameter) { req -> String in
+        let count = try req.parameters.next(Int.self)
+        return "count: \(count)"
+    }
+    
+    router.post(UserInfoData.self, at: "user-info") { (req, data) in
+        return "Hello \(data.name), your are \(data.age)"
+    }
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
+}
+
+struct CountJSON: Content {
+    let count: Int
+}
+
+struct UserInfoData: Content {
+    let name: String
+    let age: Int
 }
