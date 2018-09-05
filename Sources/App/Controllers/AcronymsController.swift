a117fdf6d3dccbd5b45dc251d58c1e67c916cb47
leiguang <575403441@qq.com>
2018-09-06 01:24:27 +0800

    Finishing CRUD

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -8,10 +8,14 @@
 import Vapor
 
 struct AcronymsController: RouteCollection {
+    
     func boot(router: Router) throws {
         let acronymsRoute = router.grouped("api", "acronyms")
         acronymsRoute.get(use: getAllHandler)
         acronymsRoute.post(use: createHandler)
+        acronymsRoute.get(Acronym.parameter, use: getHandler)
+        acronymsRoute.delete(Acronym.parameter, use: deleteHandler)
+        acronymsRoute.put(Acronym.parameter, use: updateHandler)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
@@ -22,4 +26,25 @@ struct AcronymsController: RouteCollection {
         let acronym = try req.content.decode(Acronym.self)
         return acronym.save(on: req)
     }
+    
+    func getHandler(_ req: Request) throws -> Future<Acronym> {
+        return try req.parameters.next(Acronym.self)    // 会根据id从数据库从查询
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(Acronym.self).flatMap(to: HTTPStatus.self) { acronym in
+            return acronym.delete(on: req).transform(to: .noContent)
+        }
+    }
+    
+    func updateHandler(_ req: Request) throws -> Future<Acronym> {
+        return try flatMap(to: Acronym.self, req.parameters.next(Acronym.self), req.content.decode(Acronym.self)) { acronym, updatedAcronym in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req)
+        }
+        
+    }
 }
+
+extension Acronym: Parameter {}
